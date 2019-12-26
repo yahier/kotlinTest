10a7519fe4722b4206c26c27ab15111097692627
xuziwang <xuziwang@cdoboe.com>
2019-12-26 21:05:02 +0800

    增加androidX的一些测试

---
+++ app/src/main/java/com/example/kotlintest/grammer/androidx/Text.kt
@@ -0,0 +1,17 @@
+package com.example.kotlintest.grammer.androidx
+
+/**
+ *   参考 https://developer.android.google.cn/reference/kotlin/androidx/core/text/package-summary?hl=zh_cn
+ */
+
+fun main() {
+    testCharSequence()
+}
+
+fun testCharSequence() {
+    val idNo: String = " "
+    val isBlank = idNo.isBlank()  //true
+    val isEmpty = idNo.isEmpty()  //false
+    println("testCharsquence  $isBlank $isEmpty")
+}
+
